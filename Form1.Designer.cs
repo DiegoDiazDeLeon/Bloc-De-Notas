5c0c05e5b7e5072067ac069e562b7a003e80b043
DiegoDiazDeLeon <diegodiazdeleoncai@gmail.com>
2020-09-25 21:32:58 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Bloc_De_Notas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aRCHIVOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBRIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gUARDARToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.gUARDARCOMOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gUARDARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOPIARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cORTARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pEGARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bUSQUEDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nUEVOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aRCHIVOToolStripMenuItem,
+            this.gUARDARToolStripMenuItem,
+            this.bUSQUEDAToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(670, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aRCHIVOToolStripMenuItem
+            // 
+            this.aRCHIVOToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBRIRToolStripMenuItem,
+            this.gUARDARToolStripMenuItem1,
+            this.gUARDARCOMOToolStripMenuItem,
+            this.nUEVOToolStripMenuItem,
+            this.sALIRToolStripMenuItem});
+            this.aRCHIVOToolStripMenuItem.Name = "aRCHIVOToolStripMenuItem";
+            this.aRCHIVOToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.aRCHIVOToolStripMenuItem.Text = "ARCHIVO";
+            // 
+            // aBRIRToolStripMenuItem
+            // 
+            this.aBRIRToolStripMenuItem.Name = "aBRIRToolStripMenuItem";
+            this.aBRIRToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBRIRToolStripMenuItem.Text = "ABRIR";
+            this.aBRIRToolStripMenuItem.Click += new System.EventHandler(this.aBRIRToolStripMenuItem_Click);
+            // 
+            // gUARDARToolStripMenuItem1
+            // 
+            this.gUARDARToolStripMenuItem1.Name = "gUARDARToolStripMenuItem1";
+            this.gUARDARToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.gUARDARToolStripMenuItem1.Text = "GUARDAR";
+            this.gUARDARToolStripMenuItem1.Click += new System.EventHandler(this.gUARDARToolStripMenuItem1_Click);
+            // 
+            // gUARDARCOMOToolStripMenuItem
+            // 
+            this.gUARDARCOMOToolStripMenuItem.Name = "gUARDARCOMOToolStripMenuItem";
+            this.gUARDARCOMOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gUARDARCOMOToolStripMenuItem.Text = "GUARDAR COMO";
+            this.gUARDARCOMOToolStripMenuItem.Click += new System.EventHandler(this.gUARDARCOMOToolStripMenuItem_Click);
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sALIRToolStripMenuItem.Text = "SALIR";
+            this.sALIRToolStripMenuItem.Click += new System.EventHandler(this.sALIRToolStripMenuItem_Click);
+            // 
+            // gUARDARToolStripMenuItem
+            // 
+            this.gUARDARToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOPIARToolStripMenuItem,
+            this.cORTARToolStripMenuItem,
+            this.pEGARToolStripMenuItem});
+            this.gUARDARToolStripMenuItem.Name = "gUARDARToolStripMenuItem";
+            this.gUARDARToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.gUARDARToolStripMenuItem.Text = "COPIAR";
+            // 
+            // cOPIARToolStripMenuItem
+            // 
+            this.cOPIARToolStripMenuItem.Name = "cOPIARToolStripMenuItem";
+            this.cOPIARToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cOPIARToolStripMenuItem.Text = "COPIAR";
+            this.cOPIARToolStripMenuItem.Click += new System.EventHandler(this.cOPIARToolStripMenuItem_Click);
+            // 
+            // cORTARToolStripMenuItem
+            // 
+            this.cORTARToolStripMenuItem.Name = "cORTARToolStripMenuItem";
+            this.cORTARToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cORTARToolStripMenuItem.Text = "CORTAR";
+            this.cORTARToolStripMenuItem.Click += new System.EventHandler(this.cORTARToolStripMenuItem_Click);
+            // 
+            // pEGARToolStripMenuItem
+            // 
+            this.pEGARToolStripMenuItem.Name = "pEGARToolStripMenuItem";
+            this.pEGARToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pEGARToolStripMenuItem.Text = "PEGAR";
+            this.pEGARToolStripMenuItem.Click += new System.EventHandler(this.pEGARToolStripMenuItem_Click);
+            // 
+            // bUSQUEDAToolStripMenuItem
+            // 
+            this.bUSQUEDAToolStripMenuItem.Name = "bUSQUEDAToolStripMenuItem";
+            this.bUSQUEDAToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.bUSQUEDAToolStripMenuItem.Text = "BUSQUEDA";
+            this.bUSQUEDAToolStripMenuItem.Click += new System.EventHandler(this.bUSQUEDAToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(670, 346);
+            this.textBox1.TabIndex = 1;
+            // 
+            // nUEVOToolStripMenuItem
+            // 
+            this.nUEVOToolStripMenuItem.Name = "nUEVOToolStripMenuItem";
+            this.nUEVOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nUEVOToolStripMenuItem.Text = "NUEVO";
+            this.nUEVOToolStripMenuItem.Click += new System.EventHandler(this.nUEVOToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 370);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "BLOCK DE NOTAS";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aRCHIVOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gUARDARToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem aBRIRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gUARDARToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem gUARDARCOMOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOPIARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cORTARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pEGARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bUSQUEDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nUEVOToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Bloc_De_Notas
+{
+    
+    public partial class Form1 : Form
+    {
+        bool change = true;
+        string memo;
+        string name;
+        string palabra;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gUARDARToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string contenido = textBox1.Text;
+            //si es true es decir que es nuevo
+            if (change == true)
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.Filter = "Archivos de texto|*.txt";
+                DialogResult res = dlg.ShowDialog();
+
+
+                if (res == DialogResult.OK)
+                    File.WriteAllText(dlg.FileName, contenido);
+                name = dlg.FileName;
+                change = false;
+            }
+            //si no solo guarda y sobrescribe
+            else {
+                File.WriteAllText(name,contenido);
+                memo = textBox1.Text;
+                change = false;
+            }
+            
+            
+        }
+
+        private void aBRIRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //si esta en true y no tiene nada solo lo abre
+
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "Archivos de texto|*.txt";
+                DialogResult res = dlg.ShowDialog();
+
+                if (res == DialogResult.OK)
+                    textBox1.Text = File.ReadAllText(dlg.FileName);
+                    name = dlg.FileName;
+                    change = false;
+                    memo = textBox1.Text;
+            
+
+            
+
+        }
+
+        private void gUARDARCOMOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string contenido = textBox1.Text;
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Archivos de texto|*.txt";
+            DialogResult res = dlg.ShowDialog();
+
+
+            if (res == DialogResult.OK)
+            {
+                File.WriteAllText(dlg.FileName, contenido);
+                change = false;
+            }
+
+            memo = textBox1.Text;
+            
+        }
+
+        private void sALIRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //memo comprueba que halla cambos de texto
+            if(memo != textBox1.Text)
+            {
+                change = true;
+            }
+
+            //cambios al salir 
+            if (change == false)
+            {
+                this.Close();
+            }
+            else {
+                
+                string message = "¿Deseas salir sin guardar?";
+                string caption = " No se han guardado cambios ";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+            
+            
+        }
+
+        // -----------------BUSQUEDA----------//
+        private void bUSQUEDAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            Buscador buscador = new Buscador();
+            DialogResult res = buscador.ShowDialog();
+
+            if (res == DialogResult.OK)
+            {
+                palabra = buscador.word;
+
+                string cad = palabra;
+                int resl = cad.Length;
+                int res0 = textBox1.Text.IndexOf(palabra);
+
+                if (res0 != -1)
+                    textBox1.Select(res0, resl);
+
+                else
+                    MessageBox.Show("No se encontro la Palabra buscada", "Error Al Buscar");
+
+            }
+        }
+
+        //COPIAR.
+        private void cOPIARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Copy();
+        }
+
+        //PEGAR
+        private void pEGARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Paste();
+        }
+        //CORTAR
+        private void cORTARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Cut();
+        }
+
+        private void nUEVOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
